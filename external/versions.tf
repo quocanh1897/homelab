3b1f8c0d93ac1615893d407db1dde841df2447d0
Alex Nguyen <quocanh1897@gmail.com>
2024-08-12 14:03:03 +0000

    re configure

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "binscode"
 
     workspaces {
       name = "homelab-external"
